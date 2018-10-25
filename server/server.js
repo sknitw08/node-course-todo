7bda04643cdadb0f4d8ba8ca8598ec7591ad1985
Sanjay Kumar <sank@microsoft.com>
2018-10-25 23:15:44 +0530

    Initial commit with POST request

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const {mongoose} = require('./db/mongoose');
+const {Todo} = require('./models/todo');
+const {User} = require('./models/user');
+
+let app = express();
+
+app.use(bodyParser.json()); // middleware which gives body part to request
+
+app.post('/todos', (req, res) => {
+    let todo = new Todo({
+        text: req.body.text
+    });
+    todo.save().then((doc)=>{
+        res.status(200).send(doc);
+    }, (e)=>{
+        res.status(400).send(e);
+    });
+});
+
+app.listen(3000, ()=> {
+    console.log('Started on port 3000');
+})
