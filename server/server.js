5d48adce5736128293f21683fdd8ffb8188afe1b
Sanjay Kumar <sank@microsoft.com>
2018-10-27 03:03:45 +0530

    Add test for POST /users/login

--- server/server.js
+++ server/server.js
@@ -108,7 +108,7 @@ app.post('/users/login', (req, res) => {
     User.findByCredentials(body.email, body.password).then((user) => {
         return user.generateAuthToken().then((token)=>{
             res.header('x-auth', token).send(user);
-        })
+        });
     }).catch((e)=>{
         res.status(400).send();
     })
